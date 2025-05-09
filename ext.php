19b47be288dabf5ec1d1da6a6fce9d5e090f4f19
Meisam Nobari <meis2m@yahoo.com>
2025-05-09 22:53:53 +0330

    Update ext.php

--- ext.php
+++ ext.php
@@ -2,7 +2,6 @@
 /**
 *
 * @package Ultimate phpBB SEO Friendly URL
-* @version $$
 * @copyright (c) 2017 www.phpBB-SEO.ir
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
@@ -10,11 +9,10 @@
 
 namespace phpbbseo\usu;
 
-/**
-* ext Class
-* www.phpBB-SEO.ir
-* @package Ultimate phpBB SEO Friendly URL
-*/
 class ext extends \phpbb\extension\base
 {
+    public function is_enableable()
+    {
+        return version_compare(PHP_VERSION, '7.4', '>=');
+    }
 }
